60efedf86d4305ed4d12bfc2d49d0fe29f64f80e
frizbee19 <46609035+frizbee19@users.noreply.github.com>
2023-07-28 18:42:54 -0500

    video and overlay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Contact from './Contact';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import GameJam from './Pages/GameJam';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/contact" element={<Contact />} />
+        <Route path="/drawntoreality" element={<GameJam />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/Pages/GameJam.js
@@ -0,0 +1,44 @@
+import '../App.css';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function GameJam() {
+  return (
+    <body>
+      {/* Navbar */}
+      <nav className={'navbar'} style={{ background: 'transparent' }}>
+        <div className="nav-links-container">
+          <Link className='nav-button' to="/">Home</Link>
+        </div>
+      </nav>
+      <div className='App'>
+        <header className="App-main">
+          <h2 className='Subtitle' style={{ marginTop: '2em' }}>Drawn to Reality</h2>
+        </header>
+        <div style={{ justifyContent: 'center', display: 'flex' }}>
+          <div style={{
+            position: 'relative',
+            width: '960px',
+            height: '590px',
+            overflow: 'hidden'
+          }}>
+            <iframe src="https://itch.io/embed-upload/8401986?color=333333" allowfullscreen="" width="1280" height="740" frameborder="0" style={{
+              border: 'none',
+              position: 'relative',
+              top: '0px', // Adjust these values to hide the unwanted elements
+              left: '0px',
+              width: '960px',
+              height: '640px'
+            }} />
+          </div>
+        </div>
+        <header className="App-main">
+
+        </header>
+      </div>
+    </body>
+  );
+}
+
+
+export default GameJam;
